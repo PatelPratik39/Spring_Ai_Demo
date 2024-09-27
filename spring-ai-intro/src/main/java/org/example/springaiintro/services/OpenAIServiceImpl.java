990626e0cb6a642d2a4dcaf296085944917f5247
PatelPratik39 <ptl.pratik717@gmail.com>
2024-09-26 21:29:07 -0500

    openAIService class with single method

--- spring-ai-intro/src/main/java/org/example/springaiintro/services/OpenAIServiceImpl.java
+++ spring-ai-intro/src/main/java/org/example/springaiintro/services/OpenAIServiceImpl.java
@@ -1,6 +1,9 @@
 package org.example.springaiintro.services;
 
 import org.springframework.ai.chat.model.ChatModel;
+import org.springframework.ai.chat.model.ChatResponse;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.chat.prompt.PromptTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -14,6 +17,9 @@ public class OpenAIServiceImpl implements OpenAIService {
 
     @Override
     public String getAnswer(String question) {
-        return "";
+        PromptTemplate promptTemplate = new PromptTemplate(question);
+        Prompt prompt = promptTemplate.create();
+        ChatResponse chatResponse = chatModel.call(prompt);
+        return chatResponse.getResult().getOutput().getContent();
     }
 }
