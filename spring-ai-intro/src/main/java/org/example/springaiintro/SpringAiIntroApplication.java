cc588cc044400dfa054e2abbe89d35823d41e871
PatelPratik39 <ptl.pratik717@gmail.com>
2024-09-26 20:49:49 -0500

    first commit

---
+++ spring-ai-intro/src/main/java/org/example/springaiintro/SpringAiIntroApplication.java
@@ -0,0 +1,13 @@
+package org.example.springaiintro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAiIntroApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAiIntroApplication.class, args);
+    }
+
+}
---
+++ spring-ai-intro/src/test/java/org/example/springaiintro/SpringAiIntroApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.springaiintro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAiIntroApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
