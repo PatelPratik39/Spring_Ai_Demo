14b4058b9b2807192c9f7007b4930ca571f37cb9
PatelPratik39 <ptl.pratik717@gmail.com>
2024-09-26 21:43:41 -0500

    Junit test case for OpenAI Service using some prompts

--- spring-ai-intro/src/test/java/org/example/springaiintro/services/OpenAIServiceImplTest.java
+++ spring-ai-intro/src/test/java/org/example/springaiintro/services/OpenAIServiceImplTest.java
@@ -14,7 +14,8 @@ class OpenAIServiceImplTest {
 
     @Test
     void getAnswer() {
-        String answer = openAIService.getAnswer("Write a python script to output numbers from 1 to 100");
+//        String answer = openAIService.getAnswer("Write a python script to output numbers from 1 to 100");
+        String answer = openAIService.getAnswer("Write the game snake in python");
         assertNotNull(answer, "Answer should not be null");
         System.out.println("Got the Answer :" + answer);
         System.out.println(answer);
